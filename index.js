265643655ad05b58069f1aa800268e94a2b4c06a
ahorikawa <horikawa.akihiro0202@gmail.com>
2024-02-23 11:14:45 +0900

    index.jsを変更

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+
+// app.get('/', (req, res) => {
+//   res.send('請求書作成Webサービスへようこそ！　このサービスであなたは請求書を作ることができます！');
+// });
+
+// app.listen(port, () => {
+//   console.log(`サーバーがポート ${port} で起動しました。`);
+// });
+
 const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.get('/', (req, res) => {
-  res.send('請求書作成Webサービスへようこそ！　このサービスであなたは請求書を作ることができます！');
+    // 仮の請求書データ
+    const invoice = {
+        customer: "株式会社NEXT",
+        invoiceNumber: 123456,
+        invoiceDate: "2024-02-21",
+        dueDate: "2024-03-23",
+        companyInfo: "NEXTs株式会社",
+        items: [
+            { name: "商品A", quantity: 2.5, unit: "個", unitPrice: 1000, taxRate: 10, amount: 2500 },
+            { name: "商品B", quantity: 1.0, unit: "個", unitPrice: 2000, taxRate: 8, amount: 2000 }
+        ],
+        getTotalAmount: function() {
+            return this.items.reduce((total, item) => total + item.amount, 0);
+        }
+    };
+
+    res.render('invoice', { invoice: invoice });
 });
 
 app.listen(port, () => {
-  console.log(`サーバーがポート ${port} で起動しました。`);
+    console.log(`Server running at http://localhost:${port}`);
 });
