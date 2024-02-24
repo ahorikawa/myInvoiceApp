a37d16c6f456bb81a5691c6171df188aed6c3609
ahorikawa <horikawa.akihiro0202@gmail.com>
2024-02-24 11:27:15 +0900

    index.js 不要なコードの削除

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-// const express = require('express');
-// const app = express();
-// const port = 3000;
-
-// app.get('/', (req, res) => {
-//   res.send('請求書作成Webサービスへようこそ！　このサービスであなたは請求書を作ることができます！');
-// });
-
-// app.listen(port, () => {
-//   console.log(`サーバーがポート ${port} で起動しました。`);
-// });
-
 const express = require('express');
 const app = express();
 const port = 3000;
